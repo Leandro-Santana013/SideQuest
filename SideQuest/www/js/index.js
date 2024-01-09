9e9b13d4c9bf0b8625a0e3c25b0adb66ab851d8c
Leandro Santana <ls1393424@gmail.com>
2024-01-09 20:20:16 -0300

    primeiro commit

---
+++ SideQuest/www/js/index.js
@@ -0,0 +1,36 @@
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
+
+
+const tela = new IntersectionObserver((entradas)=>{
+    entradas.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show")
+        }
+        else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const imagens = document.querySelectorAll('.hidden')
+imagens.forEach((element) => tela.observe(element))
+
+const tela1 = new IntersectionObserver((entradas1)=>{
+    entradas1.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show1")
+        }
+        else{
+            entry.target.classList.remove('show1')
+        }
+    })
+})
+
+const itens = document.querySelectorAll('.hidden1')
+itens.forEach((element) => tela1.observe(element))
